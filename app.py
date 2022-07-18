600649ee8c68e5b91cc606cae39d37af1676d2c3
123yoshidandy <56680821+123yoshidandy@users.noreply.github.com>
2022-07-19 00:14:39 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return 'hello, world'
+
+@app.route('/key1')
+def key1():
+    return os.environ['key1']
 
 if __name__ == '__main__':
     app.run()
