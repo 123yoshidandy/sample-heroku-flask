1d6d785487694b6bc3ecc76306151564985faa8d
123yoshidandy <56680821+123yoshidandy@users.noreply.github.com>
2022-07-24 14:33:04 +0900

    Update app2.py

--- sample/app2.py
+++ sample/app2.py
@@ -1,15 +1,15 @@
 from flask import Flask
 import os
 
-app3 = Flask(__name__)
+app = Flask(__name__)
 
-@app3.route('/')
+@app.route('/')
 def index():
     return 'hello, world2'
 
-@app3.route('/key1')
+@app.route('/key1')
 def key1():
     return os.environ['key1']
 
 if __name__ == '__main__':
-    app3.run()
+    app.run()
